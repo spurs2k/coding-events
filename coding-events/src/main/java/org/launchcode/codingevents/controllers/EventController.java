48a15dc93a76f5dbe9df38b4003abd0e6e119cdc
spurs2k <spurlinjames@gmail.com>
2021-02-23 20:08:58 -0600

    Second Commit

--- coding-events/src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ coding-events/src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -3,7 +3,9 @@ package org.launchcode.codingevents.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,13 +14,11 @@ import java.util.List;
 @RequestMapping("events")
 public class EventController {
 
+    private static List<String> events = new ArrayList<>();
+
 
     @GetMapping
     public String displayAllEvents(Model model) {
-        List<String> events = new ArrayList<>();
-        events.add("Code All Day 'Er Day");
-        events.add("Find a Job");
-        events.add("Make Dat Money");
         model.addAttribute("events", events);
         return "events/index";
     }
@@ -27,6 +27,13 @@ public class EventController {
     @GetMapping("create")
     public String displayCreateEventForm() {
         return "events/create";
+    }
+
+    // lives at /events/create
+    @PostMapping("create")
+    public String createEvent(@RequestParam String eventName) {
+        events.add(eventName);
+        return "redirect:";
     }
 
 }
