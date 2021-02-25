9ad0f2902d8cbb600cbabf6b1311f0791ad44c54
spurs2k <spurlinjames@gmail.com>
2021-02-24 18:22:40 -0600

    Created Model Class

--- coding-events/src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ coding-events/src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -1,5 +1,6 @@
 package org.launchcode.codingevents.controllers;
 
+import org.launchcode.codingevents.models.Event;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,29 +11,29 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.util.ArrayList;
 import java.util.List;
 
+
 @Controller
 @RequestMapping("events")
 public class EventController {
 
-    private static List<String> events = new ArrayList<>();
-
+    private static List<Event> events = new ArrayList<>();
 
     @GetMapping
     public String displayAllEvents(Model model) {
+        model.addAttribute("title", "All Events");
         model.addAttribute("events", events);
         return "events/index";
     }
 
-    // lives at /events/create
     @GetMapping("create")
-    public String displayCreateEventForm() {
+    public String displayCreateEventForm(Model model) {
+        model.addAttribute("title", "Create Event");
         return "events/create";
     }
 
-    // lives at /events/create
     @PostMapping("create")
-    public String createEvent(@RequestParam String eventName) {
-        events.add(eventName);
+    public String processCreateEventForm(@RequestParam String eventName) {
+        events.add(new Event(eventName));
         return "redirect:";
     }
 
