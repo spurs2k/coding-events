822b648c4f9e291da5f7a2c6ac69107b7db84755
spurs2k <spurlinjames@gmail.com>
2021-03-03 18:58:44 -0600

    Added Validation to name, email, description

--- coding-events/src/main/java/org/launchcode/codingevents/models/Event.java
+++ coding-events/src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,7 +1,9 @@
 package org.launchcode.codingevents.models;
 
-import org.springframework.boot.convert.DataSizeUnit;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.Objects;
 
 public class Event {
@@ -9,13 +11,21 @@ public class Event {
     private int id;
     private static int nextId = 1;
 
-
+    @NotBlank
+    @Size(min =3, max = 50, message = "Name must be between 3 and 50 characters.")
     private String name;
+
+    @Size(max = 500, message = "Description too long!")
     private String description;
 
-    public Event(String name, String description) {
+    @NotBlank
+    @Email(message = "Invalid email. Try again.")
+    private String contactEmail;
+
+    public Event(String name, String description, String contactEmail) {
         this.name = name;
         this.description = description;
+        this.contactEmail = contactEmail;
         this.id = nextId;
         nextId++;
     }
@@ -34,6 +44,14 @@ public class Event {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public String getContactEmail() {
+        return contactEmail;
+    }
+
+    public void setContactEmail(String contactEmail) {
+        this.contactEmail = contactEmail;
     }
 
     public int getId() {
